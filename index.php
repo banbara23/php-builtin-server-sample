0eed44b15ab1797e6b2abd82a26edb9d8d43b204
ikemura <k_ikemura@yumemi.co.jp>
2018-08-17 13:43:25 +0900

    memoパスにパラメーターに対応

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
+if (!preg_match('#\A/memo/(?P<id>\d+)\z#', $_SERVER['PATH_INFO'], $matches)) {
+  header('HTTP/1.1 404 Not Found');
+}
 $response = [
-  'id'=> 1,
-  'memo' => 'mock api memo',
+  'id'=> (int)$matches['id'],
+  'memo' => 'memo' . $matches['id'],
 ];
 header('Content-Typej: application/json; charset=utf-8');
 echo json_encode($response);
